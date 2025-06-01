97e40016acdb819f2018a360c1f499455b28e431
Dorffdaniel <danielcontabill@hotmail.com>
2025-06-01 12:28:10 +0100

    localizaçao adicionada

--- src/js/index.js
+++ src/js/index.js
@@ -25,3 +25,22 @@ hamburguer.addEventListener('click', ()=>{
     hamburguer.classList.toggle('ativo');
     navList.classList.toggle('ativado');
 })
+
+
+ function mostrarMapa(){
+    $('#modalLocalizacao').modal('show')
+
+     let map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+   var marker = L.marker([51.5, -0.09]).addTo(map);
+
+
+    marker.bindPopup("<b>Londres</b><br>Estamos em londres.").openPopup();
+ }
+
+ mostrarMapa();
